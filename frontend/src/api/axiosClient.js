6979ff4a00346145ac0a1a32610749b7af1145a8
SahilGore83 <sahilgore190@gmail.com>
2026-07-01 22:52:14 +0530

    Fix Python requirements typo

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://secure-chat-application-using-quantum.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
